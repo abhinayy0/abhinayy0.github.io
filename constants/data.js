797c437198e98a26059cae773c9c3e8c77fce302
Abhinay Yadav <abhinayy0@gmail.com>
2022-02-09 00:16:52 +0530

    Added spa

---
+++ constants/data.js
@@ -0,0 +1,88 @@
+const userData = {
+  githubUsername: "abhinayy0",
+  name: "Abhinay Yadav",
+  designation: "Backend Developer",
+  avatarUrl: "/avatar.png",
+  email: "abhinayy0@gmail.com",
+  phone: "+91 9520167479",
+  address: "UP, India.",
+  projects: [
+    {
+      title: "DQRCG",
+      link: "https://github.com/abhinayy0/DQRCG",
+      imgUrl:
+        "https://raw.githubusercontent.com/abhinayy0/DQRCG/main/screenshots/generatedqr.png",
+    },
+    {
+      title: "Quotes",
+      link: "https://placeholdertech.in",
+      imgUrl: "/quotes.png",
+    },
+    {
+      title: "Downloader",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "Torrent",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Codegrader",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Coderstrings",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that enjoys writing code and building products and web applications.",
+    description: [
+      `I've been in love with technology since my childhood. I've been fascinated by gadgets and technical stuff how we can make computers do our bidding by giving it instructions.`,
+      `After learning basics about computers I wanted to know more about how computer networks and operating systems work. `,
+      `I enjoy writing code, backend and network applications.I am also interested in software design and architecture.`,
+    ],
+    currentProject: "",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Backend Developer",
+      company: "Wobot",
+      year: "2021",
+      companyLink: "https://wobot.ai",
+      desc: "Developed infrastructure api's. Helped scale the platform for client onboarding. Developed backend microservices using Flask and Fastapi.",
+    },
+    {
+      title: "Project Analyst",
+      company: "Electrif AI",
+      year: "2020",
+      companyLink: "https://electrifai.net",
+      desc: "Developed api's using Flask. Worked on spend analysis project and ETL flows using python and qlikview.",
+    },
+
+    {
+      title: "Post Graduation",
+      company: "UIET, CSJMU, Kanpur",
+      year: "2019",
+      companyLink: "http://csjmuniersity.org/IT.html",
+      desc: "Masters in Computer Applications with a CGPA of 7.6.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "#",
+    twitter: "#",
+    linkedin: "https://linkedin.com/in/abhinayy0",
+    github: "https://github.com/abhinayy0",
+    facebook: "#",
+  },
+};
+
+export default userData;
